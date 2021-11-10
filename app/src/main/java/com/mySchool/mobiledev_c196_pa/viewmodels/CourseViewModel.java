104aab45e351fedf488e907dd682f4e7eda10a80
Chris <smithc238@gmail.com>
2021-11-10 17:59:38 -0500

    Added workingList and methods.

--- app/src/main/java/com/mySchool/mobiledev_c196_pa/viewmodels/CourseViewModel.java
+++ app/src/main/java/com/mySchool/mobiledev_c196_pa/viewmodels/CourseViewModel.java
@@ -5,22 +5,26 @@ import android.app.Application;
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 
 import com.mySchool.mobiledev_c196_pa.data.entities.Assessment;
 import com.mySchool.mobiledev_c196_pa.data.entities.Course;
 import com.mySchool.mobiledev_c196_pa.data.entities.Instructor;
 import com.mySchool.mobiledev_c196_pa.data.repository.CourseRepo;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class CourseViewModel extends AndroidViewModel {
     private CourseRepo repo;
     private LiveData<List<Course>> allCourses;
+    private MutableLiveData<List<Course>> workingList;
 
     public CourseViewModel(@NonNull Application application) {
         super(application);
         repo = new CourseRepo(application);
         allCourses = repo.getAllCourses();
+        workingList = new MutableLiveData<>(new ArrayList<>());
     }
 
     public long insert(Course course) {
@@ -53,5 +57,28 @@ public class CourseViewModel extends AndroidViewModel {
 
     public LiveData<List<Assessment>> getAssociatedAssessments(long id) {
         return repo.getAssociatedAssessments(id);
+    }
+
+    public void addToWorkingList(Course course) {
+        workingList.getValue().add(course);
+    }
+
+    public void setWorkingList(List<Course> courses) {
+        workingList.setValue(courses);
+    }
+
+    public void removeFromWorkingList(Course course) {
+        workingList.getValue().removeIf(item ->
+                item.getCourseID() == course.getCourseID());
+    }
+
+    public void updateFKs(long termId, List<Course> courses) {
+        for (Course course : courses) {
+            course.setTermID(termId);
+        }
+    }
+
+    public LiveData<List<Course>> getWorkingList() {
+        return workingList;
     }
 }
